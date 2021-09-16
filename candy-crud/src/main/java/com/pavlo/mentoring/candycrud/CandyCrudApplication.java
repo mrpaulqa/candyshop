9eb8499fa056431c9d3951397af755e068b3fcfa
mrpaulqa <mailforworkschool@gmail.com>
2021-09-16 17:53:41 +0200

    clean version

--- candy-crud/src/main/java/com/pavlo/mentoring/candycrud/CandyCrudApplication.java
+++ candy-crud/src/main/java/com/pavlo/mentoring/candycrud/CandyCrudApplication.java
@@ -1,4 +1,5 @@
-package com.mentoring.candycrud;
+package com.pavlo.mentoring.candycrud;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
