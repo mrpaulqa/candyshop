d2bfaf0e36e8d74b1a73a313aa24f0e1d5381e72
mrpaulqa <mailforworkschool@gmail.com>
2021-09-17 12:36:15 +0200

    1

--- candy_crud/src/main/java/com/candyshop/CandyCrudApplication.java
+++ candy_crud/src/main/java/com/candyshop/CandyCrudApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-
 public class CandyCrudApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CandyCrudApplication.class, args);
